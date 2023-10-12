8918bff8b4ad6ab637f19a6d5d66d3a3421e2f8f
César Tzoc <csartalva01@gmail.com>
2023-10-12 00:19:46 -0600

    feat: Improving the ShippingPageAdvice
    Including when the body is a ShippingPage or a ResponseEntity of a ShippingPage

--- src/main/java/com/modela/shipping/adm/controller/ShippingPageAdvice.java
+++ src/main/java/com/modela/shipping/adm/controller/ShippingPageAdvice.java
@@ -4,6 +4,7 @@ import com.modela.shipping.adm.dto.ShippingPage;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
@@ -14,16 +15,29 @@ import java.util.Collection;
 
 @ControllerAdvice
 @Slf4j
-public class ShippingPageAdvice implements ResponseBodyAdvice<ShippingPage<Collection<?>, Long>> {
+public class ShippingPageAdvice implements ResponseBodyAdvice<Object> {
 
     @Override
     public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
-        return returnType.getParameterType().isAssignableFrom(ShippingPage.class);
+        return true;
     }
 
     @Override
-    public ShippingPage<Collection<?>, Long> beforeBodyWrite(ShippingPage<Collection<?>, Long> body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
-        if (body != null) response.getHeaders().add("X-Total-Count", String.valueOf(body.count));
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+        if (body instanceof ShippingPage<?, ?>) {
+            return responseWithCollections((ShippingPage<?, ?>) body, response);
+        }
+
+        if (body instanceof ResponseEntity<?> && ((ResponseEntity<?>) body).getBody() instanceof ShippingPage<?, ?>) {
+            return responseWithCollections((ShippingPage<?, ?>) ((ResponseEntity<?>) body).getBody(), response);
+        }
+
         return body;
+    }
+
+    private Collection<?> responseWithCollections(ShippingPage<?, ?> page, ServerHttpResponse response) {
+        log.info("Including header => X-Total-Count: {}", page.count);
+        response.getHeaders().add("X-Total-Count", String.valueOf(page.count));
+        return page.items;
     }
 }
